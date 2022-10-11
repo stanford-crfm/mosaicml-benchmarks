becfa2e9783de066fa8d0b1ac6e71efdd5ba4c78
J38 <jebolton@stanford.edu>
2022-10-11 02:02:36 -0700

    switch to using modified hf GPT2

--- llm/llm/gpt.py
+++ llm/llm/gpt.py
@@ -8,6 +8,7 @@ Inspired by https://github.com/karpathy/minGPT/blob/master/mingpt/model.py
 
 import math
 from typing import Any, Mapping
+import json
 
 import torch
 import torch.nn as nn
@@ -16,176 +17,23 @@ from composer.metrics.nlp import LanguageCrossEntropy, Perplexity
 from composer.models.base import ComposerModel
 from flash_attn.flash_attention import FlashMHA
 
-
-class TorchCausalAttention(nn.Module):
-    def __init__(self, cfg: Mapping[str, Any], device: str = None):
-        super().__init__()
-        self.mha = nn.MultiheadAttention(
-            embed_dim=cfg.d_model,
-            num_heads=cfg.n_heads,
-            dropout=cfg.attn_pdrop,
-            bias=True,
-            batch_first=True,
-            device=device,
-        )
-        self.register_buffer(
-            "mask", torch.tril(torch.ones(cfg.max_seq_len, cfg.max_seq_len)))
-        self.mha.out_proj._is_residual = True
-
-    def forward(self, x, key_padding_mask):
-        return self.mha(x, x, x, attn_mask=self.mask, need_weights=False)
-
-
-class FlashCausalAttention(nn.Module):
-    def __init__(self, cfg: Mapping[str, Any], device: str = None):
-        super().__init__()
-        self.mha = FlashMHA(
-            embed_dim=cfg.d_model,
-            num_heads=cfg.n_heads,
-            attention_dropout=cfg.attn_pdrop,
-            bias=True,
-            batch_first=True,
-            causal=True,
-            device=device,
-        )
-        self.mha.out_proj._is_residual = True
-
-    def forward(self, x, key_padding_mask):
-        return self.mha(x,
-                        key_padding_mask=key_padding_mask,
-                        need_weights=False)
-
-
-class GPTMLP(nn.Module):
-    def __init__(self, cfg: Mapping[str, Any], device: str = None):
-        super().__init__()
-        self.mlp_up = nn.Linear(cfg.d_model,
-                                cfg.mlp_ratio * cfg.d_model,
-                                device=device)
-        self.mlp_act = nn.GELU(approximate='none')
-        self.mlp_down = nn.Linear(cfg.mlp_ratio * cfg.d_model,
-                                  cfg.d_model,
-                                  device=device)
-        self.mlp_down._is_residual = True
-
-    def forward(self, x):
-        return self.mlp_down(self.mlp_act(self.mlp_up(x)))
-
-
-class GPTBlock(nn.Module):
-    def __init__(self, cfg: Mapping[str, Any], device: str = None):
-        super().__init__()
-        self.ln_1 = nn.LayerNorm(cfg.d_model, device=device)
-        if cfg.attn_impl == 'torch':
-            self.causal_attn = TorchCausalAttention(cfg, device)
-        elif cfg.attn_impl == 'flash':
-            self.causal_attn = FlashCausalAttention(cfg, device)
-        else:
-            raise ValueError(f'Unknown attn_impl={cfg.attn_impl}')
-        self.ln_2 = nn.LayerNorm(cfg.d_model, device=device)
-        self.mlp = GPTMLP(cfg, device=device)
-        self.resid_attn_dropout = nn.Dropout(cfg.resid_pdrop, inplace=True)
-        self.resid_mlp_dropout = nn.Dropout(cfg.resid_pdrop, inplace=True)
-
-    def forward(self,
-                x: torch.Tensor,
-                key_padding_mask: torch.ByteTensor = None) -> torch.Tensor:
-        a = self.ln_1(x)
-        b, _ = self.causal_attn(a, key_padding_mask)
-        x = x + self.resid_attn_dropout(b)
-        m = self.ln_2(x)
-        n = self.mlp(m)
-        x = x + self.resid_mlp_dropout(n)
-        return x
-
-
-class GPT(nn.Module):
-    def __init__(self, cfg: Mapping[str, Any], device: str = 'meta'):
-        super().__init__()
-        self.cfg = cfg
-        self.transformer = nn.ModuleDict(
-            dict(
-                wte=nn.Embedding(cfg.vocab_size, cfg.d_model, device=device),
-                wpe=nn.Embedding(cfg.max_seq_len, cfg.d_model, device=device),
-                emb_drop=nn.Dropout(cfg.emb_pdrop, inplace=True),
-                blocks=nn.ModuleList([
-                    GPTBlock(cfg, device=device) for _ in range(cfg.n_layers)
-                ]),
-                ln_f=nn.LayerNorm(cfg.d_model, device=device),
-            ))
-        self.lm_head = nn.Linear(cfg.d_model,
-                                 cfg.vocab_size,
-                                 bias=False,
-                                 device=device)
-
-        if device != 'meta':
-            self.apply(self.param_init_fn)
-
-    def forward(self,
-                input_ids: torch.LongTensor,
-                key_padding_mask: torch.ByteTensor = None):
-        _, S = input_ids.size()
-        assert (
-            S <= self.cfg.max_seq_len
-        ), f"Cannot forward input with seq_len={S}, this model only supports seq_len<={self.cfg.max_seq_len}"
-        pos = torch.arange(0, S, dtype=torch.long,
-                           device=input_ids.device).unsqueeze(0)
-
-        tok_emb = self.transformer.wte(input_ids)
-        pos_emb = self.transformer.wpe(pos)
-        x = self.transformer.emb_drop(tok_emb + pos_emb)
-        for block in self.transformer.blocks:
-            x = block(x, key_padding_mask)
-        x = self.transformer.ln_f(x)
-        logits = self.lm_head(x)
-        return logits
-
-    # Param Initialization, needed for device='meta' fast initialization
-    def param_init_fn(self, module):
-        # Linear
-        if isinstance(module, nn.Linear):
-            torch.nn.init.normal_(module.weight,
-                                  mean=0.0,
-                                  std=self.cfg.init_std)
-            if module.bias is not None:
-                torch.nn.init.zeros_(module.bias)
-
-            if getattr(module, '_is_residual', False):
-                module.weight.data.normal_(
-                    mean=0.0,
-                    std=(self.cfg.init_std / math.sqrt(2 * self.cfg.n_layers)))
-
-        # Embedding
-        if isinstance(module, nn.Embedding):
-            torch.nn.init.normal_(module.weight,
-                                  mean=0.0,
-                                  std=self.cfg.init_std)
-
-        # LayerNorm
-        if isinstance(module, nn.LayerNorm):
-            torch.nn.init.zeros_(module.bias)
-            torch.nn.init.ones_(module.weight)
-
-    # FSDP Wrap function
-    def fsdp_wrap_fn(self, module):
-        return isinstance(module, GPTBlock)
-
-    # Activation Checkpointing
-    def activation_checkpointing_fn(self, module):
-        return isinstance(module, GPTBlock)
-
+from .hf_flash_gpt import GPT2FlashLMHeadModel
+from transformers.model.gpt2 import GPT2Config
 
 class ComposerGPT(ComposerModel):
 
     def __init__(self, cfg, device='meta'):
         super().__init__()
-        self.model = GPT(cfg, device=device)
+        # load GPT2 config from standard HF model config json
+        hf_config = GPT2Config.from_dict(json.loads(open(cfg.hf_config).read()))
+        # build model with config
+        self.model = GPT2FlashLMHeadModel(hf_config, device=device)
         self.train_metrics = {
-            'LanguageCrossEntropy': LanguageCrossEntropy(cfg.vocab_size),
+            'LanguageCrossEntropy': LanguageCrossEntropy(hf_config.vocab_size),
             'Perplexity': Perplexity(),
         }
         self.eval_metrics = {
-            'LanguageCrossEntropy': LanguageCrossEntropy(cfg.vocab_size),
+            'LanguageCrossEntropy': LanguageCrossEntropy(hf_config.vocab_size),
             'Perplexity': Perplexity(),
         }
 
