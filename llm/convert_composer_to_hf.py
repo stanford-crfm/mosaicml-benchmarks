a2335821df7312beaa0fac208f7c2d3d63d05369
J38 <jebolton@stanford.edu>
2022-10-17 17:01:39 -0700

    script for converting composer checkpoint

---
+++ llm/convert_composer_to_hf.py
@@ -0,0 +1,15 @@
+import collections
+import sys
+import torch
+
+input_path = sys.argv[1]
+output_path = sys.argv[2]
+
+composer_model = torch.load(input_path)
+print(composer_model["state"].keys())
+
+hf_model = collections.OrderedDict()
+for key in composer_model["state"]["model"]:
+    hf_model[key[-6:]] = composer_model["state"]["model"][key]
+
+torch.save(hf_model, output_path)
