e5a52457fb791b8eb2b93c5a09b6b6627f319b78
J38 <jebolton@stanford.edu>
2022-10-18 03:12:03 -0700

    properly remove model.

--- llm/convert_composer_to_hf.py
+++ llm/convert_composer_to_hf.py
@@ -9,6 +9,9 @@ composer_model = torch.load(input_path)
 
 hf_model = collections.OrderedDict()
 for key in composer_model["state"]["model"]:
-    hf_model[key[6:]] = composer_model["state"]["model"][key]
+    smaller_tensor = composer_model["state"]["model"][key]
+    hf_model[key] = smaller_tensor
+
+torch.nn.modules.utils.consume_prefix_in_state_dict_if_present(hf_model, prefix="model.")
 
 torch.save(hf_model, output_path)
