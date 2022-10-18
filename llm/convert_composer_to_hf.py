24052bcce004e1477c81d83c8250df026caddd26
J38 <jebolton@stanford.edu>
2022-10-17 17:13:57 -0700

    remove print statements

--- llm/convert_composer_to_hf.py
+++ llm/convert_composer_to_hf.py
@@ -6,13 +6,9 @@ input_path = sys.argv[1]
 output_path = sys.argv[2]
 
 composer_model = torch.load(input_path)
-print(composer_model["state"].keys())
-print(composer_model["state"]["model"].keys())
 
 hf_model = collections.OrderedDict()
 for key in composer_model["state"]["model"]:
-    print(key)
-    print(key[6:])
     hf_model[key[6:]] = composer_model["state"]["model"][key]
 
 torch.save(hf_model, output_path)
