123f6cc3f9310e498c7fc58ce4e5d030e6cc1580
J38 <jebolton@stanford.edu>
2022-10-18 23:34:26 -0700

    handle directory or full path

--- llm/convert_composer_to_hf.py
+++ llm/convert_composer_to_hf.py
@@ -1,4 +1,5 @@
 import collections
+import os
 import sys
 import torch
 
@@ -11,6 +12,12 @@ hf_model = collections.OrderedDict()
 for key in composer_model["state"]["model"]:
     smaller_tensor = composer_model["state"]["model"][key]
     hf_model[key] = smaller_tensor
+
+if not output_path.endswith("pytorch_model.pt") and not os.path.exists(output_path):
+    os.system(f"mkdir -p {output_path}")
+
+if not output_path.endswith("pytorch_model.pt"):
+    output_path = os.path.join(output_path, "pytorch_model.pt")
 
 torch.nn.modules.utils.consume_prefix_in_state_dict_if_present(hf_model, prefix="model.")
 
